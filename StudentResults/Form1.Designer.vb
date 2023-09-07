127bc18036ecbe2049764a91dc20ff190c8f7da6
nomvula <nzshabangu95@gmail.com>
2023-09-07 10:37:16 +0200

    Add project files.

---
+++ StudentResults/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnResult = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtMarks = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnResult
+        '
+        Me.btnResult.Location = New System.Drawing.Point(366, 266)
+        Me.btnResult.Name = "btnResult"
+        Me.btnResult.Size = New System.Drawing.Size(123, 46)
+        Me.btnResult.TabIndex = 0
+        Me.btnResult.Text = "Result"
+        Me.btnResult.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(302, 193)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Enter marks"
+        '
+        'txtMarks
+        '
+        Me.txtMarks.Location = New System.Drawing.Point(451, 187)
+        Me.txtMarks.Name = "txtMarks"
+        Me.txtMarks.Size = New System.Drawing.Size(100, 26)
+        Me.txtMarks.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtMarks)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnResult)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnResult As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtMarks As TextBox
+End Class
---
+++ StudentResults/Form1.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form1
+    Private Sub btnResult_Click(sender As Object, e As EventArgs) Handles btnResult.Click
+        Dim Mark As Integer
+
+        If IsNumeric(txtMarks.Text) = True Then
+            Mark = txtMarks.Text
+        Else
+            MsgBox("INVALID! ENTER A NUMBER")
+            Exit Sub
+        End If
+
+        If Mark < 0 Or Mark > 100 Then
+            MsgBox("invalid mark")
+        ElseIf Mark >= 50 Then
+            MsgBox("PASS")
+        ElseIf Mark < 50 Then
+            MsgBox("FAIL")
+        End If
+
+
+
+
+    End Sub
+End Class
